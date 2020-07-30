9ecb46a6c8648a69403ea8869bfab9443791112a
Brian B <bbetancova@gmail.com>
2020-07-30 03:44:26 +0000

    Separated Linkedlists into separate file. Created Readme. Added print function, add to beginning linked list function

--- linkedlist.py
+++ linkedlist.py
@@ -1,20 +1,47 @@
+# Implementing Linked Lists
 class Node:
   def __init__(self, data=None, next=None):
     self.data = data
     self.next = next
   
-  class LinkedList:
-    def __init__(self):
-      self.head = None
+class LinkedList:
+  def __init__(self):
+    self.head = None
 
-    def insert_value_after(self, data_after, data_to_insert):
-    # we must first iterate through the structure and find the data represented by "data after"
-      itr = self.head
-      while (itr):
-        if itr == data_after:
-          itr.next = Node(data_to_insert, itr.next)
-          break
-        itr = itr.next
+  # This method prints the contents of the linked list
+  def print(self):
+    if (self.head is None):
+      print("ERROR: This linked list is empty!")
+
+    itr = self.head
+    while(itr):
+      print (itr.data)
+      itr = itr.next
+
+    # This method finds an element by data value and inserts a given value after that element
+  def insert_value_after(self, data_after, data_to_insert):
+  # we must first iterate through the structure and find the data represented by "data after"
+    itr = self.head
+    while (itr):
+      if itr == data_after:
+        itr.next = Node(data_to_insert, itr.next)
+        break
+      itr = itr.next
+
+  # This method inserts an element at the beginning of the linked list
+  def insert_at_beginning(self, data):
+      # make a node element, and the "next" value of that node is the current head
+      node = Node(data, self.head)
+      self.head = node
+
+    # def insert_after_value(self, data_after, data_to_insert):
+        
+    # # Search for first occurance of data_after value in linked list
+    # while ()
+    # # Now insert data_to_insert after data_after node
+
+    # def remove_by_value(self, data):
+    # # Remove first node that contains data
         
 
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Data Structures in Python
+This is intended to serve as a place to keep my progress while I educate myself in data structures using Python.
+
+## Linked Lists
+[X] Instantiating a Linked List/Node
+[X] Printing elements of a Linked List
+[X] Adding elements to the beginning of a Linked List
+[ ]Adding elements to the end of a Linked List
