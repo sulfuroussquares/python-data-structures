d0bd1a80226de075b4d05880bfe5e427ad888f86
Brian B <bbetancova@gmail.com>
2020-08-06 14:00:13 +0000

    added reverse string functionality to stack

--- main.py
+++ main.py
@@ -2,16 +2,12 @@
 from linkedlist import Node, LinkedList
 from hashtable import HashTable
 from collections import deque
+from stack import reverse_string
 import replit
 
 # Test Area
 
 if __name__ == '__main__':
-  
-  example = deque()
-  example.append('item 1')
-  example.append('item 2')
-  example.append('item 3')
 
-  example.pop()
-  print(example)
+
+  
--- stack.py
+++ stack.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Real-World Use Case for Stacks: Writing a "back" button agnostically on a webpage, so that the program remembers last thing you looked at on a webpage
 
 # Stacks are LIFO (Last-In First-Out) structures. The last thing we put in them is the first thing we can remove or retrieve
@@ -20,3 +22,31 @@ print(example)
 
 # stack = deque()
 
+# Important to note that while I'm using this structure as a stack, "deque" in Python is a double-ended queue, which is a different structure entirely
+
+
+# -----------------------------------------------------#
+# COMMON CHALLENGES
+#------------------------------------------------------#
+
+# Reverse a stack:
+# reverse_string("This stack is being reversed") should return "desrever gnieb si kcats sihT"
+
+def reverse_string(argument):
+    print(argument)
+    stack = deque()
+    output_holder = ""
+
+    # Get all of the characters into the stack
+    for char in argument:
+      stack.append(char)
+
+    stack_height = len(stack)
+    idx = 0
+
+    while (idx < stack_height):
+      output_holder += (stack.pop())
+      idx += 1
+    
+
+    print(output_holder)
